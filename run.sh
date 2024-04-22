559f6c45f0af8c73c3efb808d21008eade7faa96
Mayank Aggarwal <mayankaggarwal1105@gmail.com>
2024-04-23 01:22:05 +0530

    weather

---
+++ run.sh
@@ -0,0 +1,7 @@
+echo "# weather-raect" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mayankagg1105/weather-raect.git
+git push -u origin main
